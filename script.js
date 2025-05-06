aa0271379f411f7b2512e49dafb58c16a6b1b3fb
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-06 13:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	let arr=[25,10,5,1];
+	ler res={q: 0, d: 0, n: 0, p: 0};
+	let i=0;
+	while(c!=0 && i<arr.length){
+		let count=Math.floor(c/arr[i]);
+		c-=count*arr[i];
+		if(arr[i] === 25) res.q=count;
+		if(arr[i] === 10) res.d=count;
+		if(arr[i] === 5) res.n=count;
+		if(arr[i] === 1) res.p=count;
+		i++;
+	}
+	return res;
 };
 
 // Do not the change the code below
